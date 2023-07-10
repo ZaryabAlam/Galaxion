a85957e49b3ba1a2dfcad4e0769d629467fef41e
ZaryabAlam <zaryabalam97@gmail.com>
2023-07-10 10:07:54 +0500

    Project Update

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:galaxion/screens/splash_page.dart';
 import 'package:get/get.dart';
 
-//test
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
